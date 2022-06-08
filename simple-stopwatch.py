45929950f124a793e3d5b4809f5842d3d4e8184b
Joel Ayllon <jal1984go@gmail.com>
2022-06-08 18:32:32 -0500

    return none fixed...it works as expected now.

--- simple-stopwatch.py
+++ simple-stopwatch.py
@@ -1,8 +1,7 @@
-from cgi import print_arguments
 import time
 from datetime import datetime, timedelta
 
-current_date=datetime.now()
+time_ended=datetime.now()
 
 def time_convert(sec):
   mins = sec // 60
@@ -21,6 +20,6 @@ end_time = time.time()
 time_lapsed = end_time - start_time
 time_started=timedelta(((time_lapsed/24)/60)/60)
 
-print("Time started: ",current_date-time_started)
-print(time_convert(time_lapsed))
-print("Time finished: ",current_date)
+print("Time started: ",time_ended-time_started)
+time_convert(time_lapsed)
+print("Time finished: ",time_ended)
